9aa9e284de125c3450c94a2844f25144e37cf241
nick <nick@localhost.localdomain>
2019-01-26 11:41:16 -0500

    Fix dumb movement bug.

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -6,27 +6,20 @@ var motion = Vector2()
 
 
 func get_input():
-	var moved = false;
+	motion = Vector2()
 	if Input.is_action_pressed('ui_right'):
 		motion.x += 1
-		moved = true;
-
 	if Input.is_action_pressed('ui_left'):
 		motion.x -= 1
-		moved = true;
 	if Input.is_action_pressed('ui_up'):
 		motion.y -= 1
-		moved = true;
 	if Input.is_action_pressed('ui_down'):
 		motion.y += 1
-		moved = true;
-		
-	if !moved:
-		motion.x = 0
-		motion.y = 0
 		
 	motion = motion.normalized() * SPEED
 
 func _physics_process(delta):
 	get_input()
 	move_and_collide(motion)
+	if global_position.x < 0:
+		global_position.x = 0;
